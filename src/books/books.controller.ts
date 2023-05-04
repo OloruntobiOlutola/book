bc5c16f4546ffabdf5658e9cc52464c5c979a921
Olutola Oloruntobi Paul <oloruntobiolutola@gmail.com>
2023-05-04 14:57:05 +0100

    updated

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,21 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksDto } from './dto/books.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BooksController {
+  constructor(private booksService: BooksService) {}
 
-  constructor(private booksService: BooksService ){}
-  
   @Post()
   createBook(@Body() book: BooksDto) {
-    return this.booksService.createBook(book)
+    return this.booksService.createBook(book);
   }
 
   @Get()
   getBooks() {
-    return  this.booksService.getBooks();
+    return this.booksService.getBooks();
   }
 
   @Get('/:id')
@@ -24,16 +31,12 @@ export class BooksController {
   }
 
   @Patch('/:id')
-  updateBook(
-    @Param('id') id: string,
-    @Body() body: UpdateBookDto
-  ) {
+  updateBook(@Param('id') id: string, @Body() body: UpdateBookDto) {
     return this.booksService.updateBook(id, body);
   }
 
   @Delete('/:id')
-  deleteBook (@Param('id') id: string) {
+  deleteBook(@Param('id') id: string) {
     return this.booksService.deleteBook(id);
   }
-
 }
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -5,12 +5,24 @@ import { Books } from './entity/books';
 
 describe('BooksService', () => {
   let service: BooksService;
-  let _books: Books[] ;
-  
-  beforeEach(async () => {
+  let _books: Books[];
 
+  beforeEach(async () => {
+    _books = [
+      {
+        id: '1',
+        title: 'My First Book',
+        content: 'My First Book content',
+      },
+    ];
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ BooksService ],
+      providers: [
+        BooksService,
+        {
+          provide: 'BOOK',
+          useValue: _books,
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
@@ -18,31 +30,31 @@ describe('BooksService', () => {
 
   it('getBooks should return all books', async () => {
     const books = await service.getBooks();
-    expect(books).toBeDefined;
-  })
+    expect(books.length).toBe(1);
+  });
 
-  // it('getBook should return a book given its id', async () => {
-  //   const book = await service.getBook('1')
-  //   expect(book).toEqual(
-  //     {
-  //       id: '1',
-  //       title: 'title',
-  //       content: 'content'
-  //     }
-  //   )
-  // })
+  it('getBook should return a book given its id', async () => {
+    const book = await service.getBook('1');
+    expect(book).toEqual({
+      id: '1',
+      title: 'My First Book',
+      content: 'My First Book content',
+    });
+  });
 
-  it('createBook should create a book', () => {
-    const book = service.createBook({title: 'title', content: 'content'});
+  // it('createBook should create a book', () => {
+  //   const book = service.createBook({ title: 'title', content: 'content' });
 
-    expect(book.id).toBeDefined();
-    expect(book.title).toEqual('title');
-    expect(book.content).toEqual('content');
-  })
+  //   expect(book.id).toBeDefined();
+  //   expect(book.title).toEqual('title');
+  //   expect(book.content).toEqual('content');
+  // });
 
-  it('throws an error if book id is not found', async () => {
-     expect(service.updateBook('2', {title: 'title'})).rejects.toThrow(HttpException)
-  })
+  // it('throws an error if book id is not found', async () => {
+  //   expect(service.updateBook('2', { title: 'title' })).rejects.toThrow(
+  //     HttpException,
+  //   );
+  // });
 
   // it('should update a book given a book id', async () => {
   //   const book = service.updateBook('1', {title: 'new title'});
@@ -52,5 +64,4 @@ describe('BooksService', () => {
   //       content: 'content'
   //   })
   // })
-
 });
